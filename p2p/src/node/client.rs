300bd8bc9074e12bc2bf74965bc69d7d8713029e
dorname <lgqfighting@163.com>
2024-03-08 00:16:07 +0800

    P2P节点实验

--- p2p/src/node/client.rs
+++ p2p/src/node/client.rs
@@ -1,22 +1,39 @@
 
 use std::net::TcpStream;
-use std::io::{self, Read, Write};
+use std::io::{self, Error, Read, Write};
 use std::str;
 
+struct Client;
+impl Client {
+    fn new()->Self{
+        Self{}
+    }
+    fn connect(addr:String,msg:String)-> io::Result<()>{
+        let client = Client::new();
+        let mut stream = TcpStream::connect(&addr)?;
+        println!("Successfully connected to server {}",addr);
+        client.send(msg, &mut stream);
+        let response = client.get_response(&mut stream)?;
+        println!("Reply:{}",response);
+        Ok(())
+    }
+    
+    fn send(&self,msg:String,stream:&mut TcpStream)->io::Result<()>{
+        println!("Sending {}",msg);
+        stream.write_all(msg.as_bytes())?;
+        Ok(())
+    }
+    
+    fn get_response(&self,stream:&mut TcpStream)->Result<String, Error>{
+        let mut data = [0 as u8; 50]; // Use a buffer larger than the message to receive the server's response.
+        let bytes_read = stream.read(&mut data)?;
+        let response = str::from_utf8(&data[..bytes_read]).expect("Received non-UTF8 message");
+        Ok(response.to_owned())
+    }    
+}
+
 #[test]
-fn client() -> io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:12345")?;
-    println!("Successfully connected to server in port 12345");
-
-    let msg = "Hello!\n";
-    stream.write_all(msg.as_bytes())?;
-    println!("Sent Hello, awaiting reply...");
-
-    let mut data = [0 as u8; 50]; // Use a buffer larger than the message to receive the server's response.
-    let bytes_read = stream.read(&mut data)?;
-    let response = str::from_utf8(&data[..bytes_read]).expect("Received non-UTF8 message");
-
-    println!("Reply: {}", response);
-    Ok(())
+fn test_client() {
+    Client::connect("127.0.0.1:12345".to_owned(),"Hi\n".to_owned());
 }
 
--- p2p/src/node/mod.rs
+++ p2p/src/node/mod.rs
@@ -1,2 +1,10 @@
 pub mod client;
 pub mod server;
+//p2p节点之间的建立通信
+//会话密钥
+//通信安全 
+struct Node{
+    id:String
+    
+
+}
